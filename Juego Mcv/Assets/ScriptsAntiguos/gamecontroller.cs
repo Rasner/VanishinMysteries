8004aaf251091794a0ba6108e17dae56c6d80e54
Sanca Venegas, Martin Joel <splayer003@gmail.com>
2018-07-02 00:50:10 -0500

    dmwamd
    dsada

---
+++ Juego Mcv/Assets/ScriptsAntiguos/gamecontroller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gamecontroller : MonoBehaviour {
+
+    public static gamecontroller instance;
+    public bool zona;
+	// Use this for initialization
+	void Start () {
+        instance = this;
+        zona = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
