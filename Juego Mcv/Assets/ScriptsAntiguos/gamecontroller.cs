aa871a645b4c233cde807b3e2562622da562e861
Sanca Venegas, Martin Joel <splayer003@gmail.com>
2018-07-02 05:05:21 -0500

    dsad
    dsa

--- Juego Mcv/Assets/ScriptsAntiguos/gamecontroller.cs
+++ Juego Mcv/Assets/ScriptsAntiguos/gamecontroller.cs
@@ -9,7 +9,7 @@ public class gamecontroller : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         instance = this;
-        zona = false;
+        zona = true;
 	}
 	
 	// Update is called once per frame
