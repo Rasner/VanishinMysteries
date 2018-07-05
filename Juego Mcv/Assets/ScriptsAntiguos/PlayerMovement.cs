fc6e669378c8ea4976babf26329edf4761e69282
Sanca Venegas, Martin Joel <splayer003@gmail.com>
2018-07-05 15:52:24 -0500

    sdas
    dasda

--- Juego Mcv/Assets/ScriptsAntiguos/PlayerMovement.cs
+++ Juego Mcv/Assets/ScriptsAntiguos/PlayerMovement.cs
@@ -8,28 +8,15 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     //private Rigidbody RB3D;
 
-    private void Start()
+    void Start()
     {
         //RB3D = GetComponent<Rigidbody>();
     }
 
-    private void Update()
+    void Update()
     {
-        //RB3D.AddForce(joystick.InputDirection * speed * Time.deltaTime);
         transform.Translate(speed * joystick.InputDirection * Time.deltaTime);
-        /*if (salto == 1 && jump == true)
-        {
-            transform.Translate(0, 15, 0);
-            salto = 0;
-        }
-        */
     }
-
-    /*public void OnCollisionEnter(Collision collision)
-    {
-        salto = 1;
-    }
-    */
 
     public void saltar (bool jump)
     {
